c5ef3aa8f5c94a810be8de4b2f20659ae1ba0c53
Brandon Dixon <brandon.dixon@sevun.com>
2018-09-22 13:55:01 -0600

    changed memory location to use bootloader

--- example-application/tm4c123gh6pm.cmd
+++ example-application/tm4c123gh6pm.cmd
@@ -10,7 +10,7 @@
 
 MEMORY
 {
-    FLASH (RX) : origin = 0x00000000, length = 0x00040000
+    FLASH (RX) : origin = 0x00002000, length = 0x0003D000
     SRAM (RWX) : origin = 0x20000000, length = 0x00008000
 }
 
@@ -28,7 +28,7 @@ MEMORY
 
 SECTIONS
 {
-    .intvecs:   > 0x00000000
+    .intvecs:   > 0x00002000
     .text   :   > FLASH
     .const  :   > FLASH
     .cinit  :   > FLASH
